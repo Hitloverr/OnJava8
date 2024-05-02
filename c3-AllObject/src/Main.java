8afd8a9f3aa731fe574c2d8e26c1ea4168e2aab0
xiangxiao <244847243@qq.com>
2024-05-02 20:00:52 +0800

    第三章：一切皆对象

---
+++ c3-AllObject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.getProperties().list(System.out);
+        System.getProperty("user.name");
+
+    }
+}
